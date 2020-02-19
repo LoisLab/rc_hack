05e7b198872b0315697c15d767ff833280959980
jeff-loislab <46331229+jeff-loislab@users.noreply.github.com>
2020-02-19 15:19:18 -0500

    revised for presentation

--- rc_hack.py
+++ rc_hack.py
@@ -17,40 +17,44 @@ GPIO.output(13, True)
 movetime = .5
 
 def go_fwd():
-    GPIO.output(3, True)
-    GPIO.output(5, False)
+    GPIO.output(11, True)   #one pin on
+    GPIO.output(13, False)  #one pin off
     sleep(movetime)
-    GPIO.output(3, True)
-    GPIO.output(5, True)
+    GPIO.output(11, True)   
+    GPIO.output(13, True)   
 
 def go_back():
-    GPIO.output(3, False)
-    GPIO.output(5, True)
+    GPIO.output(11, False)  #setting pins the opposite way
+    GPIO.output(13, True)   #makes the motor go the other direction
     sleep(movetime)
-    GPIO.output(3, True)
-    GPIO.output(5, True)
+    GPIO.output(11, True)
+    GPIO.output(13, True)
     
-def go_right():
-    GPIO.output(3, True)
-    GPIO.output(5, False)
-    GPIO.output(11,False)
+def go_left():
+    GPIO.output(11, True)    #steering works the same way
+    GPIO.output(13, False)
+    GPIO.output(3,False)   #moving the car while the wheels are turned
+    GPIO.output(5,True)
     sleep(movetime)
     GPIO.output(3, True)
     GPIO.output(5, True)
     GPIO.output(11, True)
+    GPIO.output(13,True)
 
-def go_left():
+def go_right():
+    GPIO.output(11, True)
+    GPIO.output(13, False)
     GPIO.output(3, True)
-    GPIO.output(5, False)
-    GPIO.output(13,False)
+    GPIO.output(5,False)
     sleep(movetime)
+    GPIO.output(11, True)
+    GPIO.output(13, True)
     GPIO.output(3, True)
     GPIO.output(5, True)
-    GPIO.output(13, True)
     
 go_fwd()
 go_fwd()
 go_right()
 go_left()
-GPIO.cleanup()
+GPIO.cleanup()              # always clean up after yourself
 
